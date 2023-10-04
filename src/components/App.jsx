232c2d142f910e126f09c085c2ad68e90a897742
Yaroslav <138517664+WithinKael@users.noreply.github.com>
2023-10-04 16:01:04 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { ContactForm } from './ContactsForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  onInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value.trim() });
+  };
+
+  onAddContact = contact => {
+    const { contacts } = this.state;
+
+    const isDuplicateName = contacts.some(
+      existingContact =>
+        existingContact.name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isDuplicateName) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  onDeletePost = postId => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== postId),
+    });
+  };
+
+  inputFilter = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.inputFilter();
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          contacts={this.state.contacts}
+          onAddContact={this.onAddContact}
+        />
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} onInputChange={this.onInputChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeletePost={this.onDeletePost}
+        />
+      </div>
+    );
+  }
+}
